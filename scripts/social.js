8f1d96d29e463735893c49e3429b0b2fb7c6c6e1
MKM <kaanmesum@gmail.com>
2023-12-29 12:41:31 +0300

    shity animation

--- scripts/social.js
+++ scripts/social.js
@@ -1,5 +1,5 @@
 let chatContainer = document.getElementsByClassName("chat-container")[0];
-
+let socialWrapper = document.getElementsByClassName("social-container")[0];
 function toggleChat() {
   if (chatContainer.classList.contains("chat-closed")) {
     chatContainer.classList.add("chat-transition");
@@ -7,10 +7,13 @@ function toggleChat() {
       chatContainer.classList.remove("chat-transition");
       chatContainer.classList.remove("chat-closed");
     }, 1000);
+    socialWrapper.classList.add("social-wrapper-open");
+    socialWrapper.classList.remove("social-wrapper-closed");
   } else {
     chatContainer.classList.add("chat-transition");
     setTimeout(() => {
       chatContainer.classList.remove("chat-transition");
+      socialWrapper.classList.add("social-wrapper-closed");
     }, 1000);
     chatContainer.classList.add("chat-closed");
   }
