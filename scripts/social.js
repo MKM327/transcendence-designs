c903165955fec5c58fbfbbe3da4484e0ac7ff3b6
MKM <kaanmesum@gmail.com>
2023-12-29 16:21:00 +0300

    Refactor chat functionality and add chat close button

--- scripts/social.js
+++ scripts/social.js
@@ -1,24 +1,31 @@
-let chatContainer = document.getElementsByClassName("chat-container")[0];
-let socialWrapper = document.getElementsByClassName("social-container")[0];
-function toggleChat() {
-  if (chatContainer.classList.contains("chat-closed")) {
-    chatContainer.classList.add("chat-transition");
-    setTimeout(() => {
-      chatContainer.classList.remove("chat-transition");
-      chatContainer.classList.remove("chat-closed");
-    }, 1000);
-    socialWrapper.classList.add("social-wrapper-open");
-    socialWrapper.classList.remove("social-wrapper-closed");
-  } else {
+function handleChatState() {
+  let chatContainer = document.getElementsByClassName("chat-container")[0];
+  let socialWrapper = document.getElementsByClassName("social-container")[0];
+  let chatCloseButton = document.getElementById("chat-close-button");
+
+  chatCloseButton.addEventListener("click", () => {
+    console.log("clicked");
     chatContainer.classList.add("chat-transition");
     setTimeout(() => {
       chatContainer.classList.remove("chat-transition");
       socialWrapper.classList.add("social-wrapper-closed");
     }, 1000);
     chatContainer.classList.add("chat-closed");
+  });
+  function toggleChat() {
+    if (chatContainer.classList.contains("chat-closed")) {
+      chatContainer.classList.add("chat-transition");
+      setTimeout(() => {
+        chatContainer.classList.remove("chat-transition");
+        chatContainer.classList.remove("chat-closed");
+      }, 1000);
+      socialWrapper.classList.add("social-wrapper-open");
+      socialWrapper.classList.remove("social-wrapper-closed");
+    }
+  }
+  let allUsers = document.getElementsByClassName("user-wrapper");
+  for (let i = 0; i < allUsers.length; i++) {
+    allUsers[i].addEventListener("click", toggleChat);
   }
 }
-let allUsers = document.getElementsByClassName("user-wrapper");
-for (let i = 0; i < allUsers.length; i++) {
-  allUsers[i].addEventListener("click", toggleChat);
-}
+handleChatState();
