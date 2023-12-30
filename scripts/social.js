3a2d905cfc1db2402fe6f10cd43c472103ec5121
MKM <kaanmesum@gmail.com>
2023-12-30 18:48:26 +0300

    animations

--- scripts/social.js
+++ scripts/social.js
@@ -8,7 +8,7 @@ function handleChatState() {
     chatContainer.classList.add("chat-transition");
     setTimeout(() => {
       chatContainer.classList.remove("chat-transition");
-      socialWrapper.classList.add("social-wrapper-closed");
+      socialWrapper.classList.add("social-wrapper-chat-closed");
     }, 1000);
     chatContainer.classList.add("chat-closed");
   });
@@ -20,7 +20,7 @@ function handleChatState() {
         chatContainer.classList.remove("chat-closed");
       }, 1000);
       socialWrapper.classList.add("social-wrapper-open");
-      socialWrapper.classList.remove("social-wrapper-closed");
+      socialWrapper.classList.remove("social-wrapper-chat-closed");
     }
   }
   let allUsers = document.getElementsByClassName("user-wrapper");
@@ -28,4 +28,23 @@ function handleChatState() {
     allUsers[i].addEventListener("click", toggleChat);
   }
 }
+function handleSelectedPost() {
+  let allCommentButtons = document.getElementsByClassName("comment-button");
+  let socialWrapper = document.getElementsByClassName("social-wrapper")[0];
+  let selectedPost = document.getElementsByClassName("selected-post")[0];
+  for (let i = 0; i < allCommentButtons.length; i++) {
+    allCommentButtons[i].addEventListener("click", () => {
+      socialWrapper.classList.add("social-wrapper-closed");
+      setTimeout(() => {
+        selectedPost.classList.add("selected-post-open");
+      }, 1000);
+    });
+  }
+  let backButton = document.getElementById("comment-back-button");
+  backButton.addEventListener("click", () => {
+    socialWrapper.classList.remove("social-wrapper-closed");
+    selectedPost.classList.remove("selected-post-open");
+  });
+}
+handleSelectedPost();
 handleChatState();
